f471f61ede5fc95e9c15494d399a78ae9df04d1d
Jules Ginsparg <ginspargjules@gmail.com>
2019-07-11 15:34:38 -0400

    Added message auto-delete over time, scrolling, and sending with enter

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
     belongs_to :user
     validates :body, presence: true
+    #this determines which messages are displayed
+    scope :custom_display, -> { order(:created_at).last(20) }
 end
