0ab4f93f79cef7c0a0f116fad9fdf37b9c2feb38
Jules Ginsparg <ginspargjules@gmail.com>
2019-07-11 11:57:40 -0400

    added authentication and the ability to log in and out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "The offering is accepted"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Incorrect username or password"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have logged out, but that is no true escape"
+    redirect_to login_path
+  end
 end
