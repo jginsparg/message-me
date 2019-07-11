b4c8886d7db7919f56c077d8ca054b330d1c74df
Jules Ginsparg <ginspargjules@gmail.com>
2019-07-11 14:59:27 -0400

    messaging now works, as does logging in - still needs layout and other updates

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  post 'message', to: 'messages#create'
+
+  mount ActionCable.server, at: '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
